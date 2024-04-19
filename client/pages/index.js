fd62a47b87ecbe62baed49bcd82b8f6d6ca1355f
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2024-04-19 17:07:33 +0300

    client ingress nginx settings

---
+++ client/pages/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    // console.log(currentUser);
+    // axios.get('/api/users/currentuser');
+
+    return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+    if (typeof window === 'undefined') {
+        const { data } = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            { headers: {
+                Host: 'ticketing.dev'
+            }}
+        );
+
+        return data;
+
+    } else {
+        const { data } = await axios.get('/api/users/currentuser');
+
+        return data;
+    }
+    return {};
+}
+
+export default LandingPage;
