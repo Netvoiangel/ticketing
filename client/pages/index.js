1fc50b5e5c6d1390c87696247da99c0df49d68cf
Verner Ravil <werner.ravil@gmail.com>
2024-05-31 18:47:20 +0500

    Test fixing client

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,19 +2,23 @@ import Link from 'next/link';
 
 
 const LandingPage = ({ currentUser, tickets }) => {
-    const ticketList = tickets.map((ticket) => {
-        return (
-            <tr key={ticket.id}>
-                <td>{ticket.title}</td>
-                <td>{ticket.price}</td>
-                <td>
-                    <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
-                        View
-                    </Link>
-                </td>
-            </tr>
-        );
-    });
+    let ticketList = null;
+    if (tickets && tickets.length > 0) {
+        ticketList = tickets.map((ticket) => {
+            return (
+                <tr key={ticket.id}>
+                    <td>{ticket.title}</td>
+                    <td>{ticket.price}</td>
+                    <td>
+                        <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+                            View
+                        </Link>
+                    </td>
+                </tr>
+            );
+        });
+    }
+
     return (
         <div>
             <h1>Tickets</h1>
