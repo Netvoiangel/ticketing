1374514ef02632d8c6e9c2a36af1cdd936f67fcf
Timur <110711364+Netvoiangel@users.noreply.github.com>
2024-05-31 10:18:08 +0300

    Update index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,19 +2,21 @@ import Link from 'next/link';
 
 
 const LandingPage = ({ currentUser, tickets }) => {
-    const ticketList = tickets.map((ticket) => {
-        return (
-            <tr key={ticket.id}>
-                <td>{ticket.title}</td>
-                <td>{ticket.price}</td>
-                <td>
-                    <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
-                        View
-                    </Link>
-                </td>
-            </tr>
-        );
-    });
+    if (tickets !== null) {
+        const ticketList = tickets.map((ticket) => {
+            return (
+                <tr key={ticket.id}>
+                    <td>{ticket.title}</td>
+                    <td>{ticket.price}</td>
+                    <td>
+                        <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+                            View
+                        </Link>
+                    </td>
+                </tr>
+            );
+        });
+    }
     return (
         <div>
             <h1>Tickets</h1>
